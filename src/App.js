902eef26037fcd95c9b864e8e5a3629de27330fd
dhiraj puri <dhirajdpuri@gmail.com>
2022-11-16 10:22:03 +0530

    Done with About & contact us page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import FullLayout from "./layout/full-layout/FullLayout";
 import Contact from "./features/frontend/contact/Contact";
 import About from "./features/frontend/about/About";
+import ImageGallary from "./features/frontend/image-gallery/ImageGallery";
 
 function App() {
   return (
     <>
       {/* <FullLayout /> */}
       <About />
-      {/* <Contact /> */}
+      {/* <ImageGallary /> */}
+      <Contact />
     </>
   );
 }
--- src/features/frontend/about/About.js
+++ src/features/frontend/about/About.js
@@ -1,142 +1,140 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
-import { Container, Typography, Box, Item, Grid } from "@mui/material";
 
 const About = () => {
   return (
-    <Box sx={{ height: "100%", width: "100%", bgcolor: "gray" }}>
-      <Typography gutterBottom varient="h1" align="center">
-        About us
-      </Typography>
-      <Container sx={{ width: "100%", bgcolor: "gray" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            p: 1,
-            m: 1,
-            bgcolor: "#ED563B",
-            height: "400px",
-          }}
-        >
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
+    <Box>
+      <Box
+        sx={{
+          color: "#ed563b",
+          fontWeight: "700",
+          letterSpacing: "2.5px",
+          fontSize: 40,
+          p: 4,
+        }}
+        align="center"
+      >
+        ABOUT
+      </Box>
+      <Box
+        mx="auto"
+        // height="400px"
+        borderRadius="10px"
+        sx={{
+          boxShadow: "5px 5px 25px rgba(0,0,0,0.2)",
+          width: { xs: "400px", md: "1000px" },
+          padding: "20px",
+          display: "flex",
+          marginBottom: "40px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <img
+          src="http://www.roadtofitness.in/img/r2f-interior.jpg"
+          style={{ width: "400px", height: "300px" }}
+        />
+        <Box>
+          <Typography
+            align="center"
+            sx={{ padding: "20px", fontWeight: 800, color: "#ed563b" }}
+            variant="h"
           >
-            <Typography sx={{ color: "white" }} variant="h5">
-              About Us:
-            </Typography>
-            <Typography variant="h6">Who Are We?</Typography>
-            <Typography>
-              We are oTypographyerating in Kamshet, Maharashtra and have been
-              successfully running for several years. We provide high-quality
-              fitness services from gym equipment to nutrition and proper
-              supplementation. We are a team of certified trainers and fitness
-              experts who are passionate about transforming your health and
-              physique. Our motto is “Get Ready To Fit!”.
-            </Typography>
-            <Typography variant="h6">Why Join our Gym?</Typography>
-            <Typography>
-              Whenever you think about Gym, we want you to think about high
-              quality. Our main objective is to provide you with a modern gym
-              that includes top-notch equipment, expert fitness trainers who
-              will guide you thoroughly until you reach your desired fitness
-              goals and a clean & hygienic workout environment.
-            </Typography>
-          </Grid>
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <img
-              src="http://www.roadtofitness.in/img/r2f-interior.jpg"
-              style={{ width: "95%", height: "90%", p: "" }}
-            />
-          </Grid>
+            ABOUT US
+          </Typography>
+          <Typography sx={{ padding: "20px" }}>
+            <p
+              style={{
+                lineHeight: "1.3",
+                textAlign: "justify",
+                textIndent: 50,
+              }}
+            >
+              Our Fitness Journey began in the year 2020 and as the name goes
+              Health and Heaven. In this short period, We have taken special
+              efforts and made it our specialty to make practical &
+              result-oriented fitness programs. We have trained dedicated
+              fitness professionals with real-world experience in the field to
+              help you achieve your desired fitness goals. And we have a key
+              factor that makes us different from others is Our Fitness Programs
+              which are developed to avoid future injuries & also we have the
+              expertise to deal with Physical Injuries. Our Yoga programs help
+              to improve mental health. We are committed to providing a healthy
+              way of life to our clients which makes our fitness studio a
+              Health's Heaven. We proud to share that 400+ members have achieved
+              their fitness goals with Health & Heaven and still counting.
+            </p>
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            p: 1,
-            m: 1,
-            bgcolor: "white",
-            height: "300px",
-          }}
-        >
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <img
-              src="https://classicgym.in/wp-content/uploads/2021/06/facilities-banner-classic-gym-1.png"
-              style={{ width: "95%", height: "90%" }}
-            />
-          </Grid>
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <Typography>Facilities:</Typography>
-            <ul>
-              <li>Changing Rooms.</li>
-              <li>Kickboxing Zone.</li>
-              <li>Lockers.</li>
-              <li>Lockers.</li>
+      </Box>
+      <Box
+        mx="auto"
+        // height="400px"
+        borderRadius="10px"
+        sx={{
+          boxShadow: "5px 5px 25px rgba(0,0,0,0.2)",
+          width: { xs: "400px", md: "1000px" },
+          padding: "20px",
+          display: "flex",
+          marginBottom: "40px",
+          flexDirection: { xs: "column", md: "row" },
+          backgroundColor: "#ed563b",
+        }}
+      >
+        <Box sx={{ color: "white" }}>
+          <Typography sx={{ padding: "20px", fontWeight: 800 }} variant="h">
+            FACILITIES
+          </Typography>
+
+          <Typography sx={{ padding: "20px" }}>
+            <ul
+              style={{
+                listStyle: "square",
+                paddingRight: "390px",
+              }}
+            >
+              <li>Changing Rooms</li>
+              <li>Kickboxing Zone</li>
+              <li>Lockers</li>
+              <li>Lockers</li>
               <li>Steambath</li>
               <li>Body massagae</li>
-              <li>Lounge Area.</li>
-              <li>Water Dispenser. </li>
-              <li>Wifi.</li>
+              <li>Lounge Area</li>
+              <li>Water Dispenser</li>
+              <li>Wifi</li>
             </ul>
-          </Grid>
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            p: 1,
-            m: 1,
-            bgcolor: "#ED563B",
-            height: "300px",
-          }}
-        >
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
+        <img
+          src="https://classicgym.in/wp-content/uploads/2021/06/facilities-banner-classic-gym-1.png"
+          style={{ width: "400px", height: "300px" }}
+        />
+      </Box>
+      <Box
+        mx="auto"
+        // height="400px"
+        borderRadius="10px"
+        sx={{
+          boxShadow: "5px 5px 25px rgba(0,0,0,0.2)",
+          width: { xs: "400px", md: "1000px" },
+          padding: "20px",
+          display: "flex",
+          marginBottom: "40px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <img
+          src="https://www.fitness-world.in/wp-content/uploads/2018/10/Blog-Banner.jpg"
+          style={{ width: "400px", height: "300px" }}
+        />
+        <Box>
+          <Typography
+            sx={{ padding: "20px", fontWeight: 800, color: "#ed563b" }}
+            variant="h"
           >
-            <Typography>Equipment:</Typography>
-
-            <ul>
+            EQUIPMENTS
+          </Typography>
+          <Typography sx={{ padding: "20px" }}>
+            <ul style={{ listStyle: "square" }}>
               <li> Training bench</li>
               <li> Dumbbell set</li>
               <li> Treadmill</li>
@@ -147,113 +145,94 @@ const About = () => {
               <li> Ellipticals</li>
               <li> Balance trainers and resistance bands</li>
             </ul>
-          </Grid>
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <img
-              src="https://www.fitness-world.in/wp-content/uploads/2018/10/Blog-Banner.jpg"
-              style={{ width: "95%", height: "90%" }}
-            />
-          </Grid>
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            p: 1,
-            m: 1,
-            bgcolor: "white",
-            height: "300px",
-          }}
-        >
-          <Grid
+      </Box>
+      <Box
+        mx="auto"
+        // height="400px"
+        borderRadius="10px"
+        sx={{
+          boxShadow: "5px 5px 25px rgba(0,0,0,0.2)",
+          width: { xs: "400px", md: "1000px" },
+          padding: "20px",
+          display: "flex",
+          marginBottom: "40px",
+          flexDirection: { xs: "column", md: "row" },
+          backgroundColor: "#ed563b",
+        }}
+      >
+        <Box sx={{ color: "white" }}>
+          <Typography sx={{ padding: "20px", fontWeight: 800 }} variant="h">
+            TRAINER
+          </Typography>
+          <Typography
             sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
+              padding: "20px",
+              paddingTop: "0",
+              textAlign: "justify",
+              lineHeight: "1.2",
             }}
-            xs={12}
-            sm={6}
           >
-            <img
-              src="https://thumbs.dreamstime.com/b/two-attractive-women-exercising-personal-trainers-two-attractive-women-exercising-personal-trainers-gym-122702382.jpg"
-              style={{ width: "95%", height: "90%" }}
-            />
-          </Grid>
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <Typography>Trainer:</Typography>
-            <Typography>
-              A gym instructor is someone whose job it is to help other people
-              improve their fitness. They are usually employed to work within a
-              specific gym, providing support to gym members, often acting as
-              the first person to show them around during their induction.
-            </Typography>
-          </Grid>
+            <p>
+              Personal Trainer & Group trainers : We have trained dedicated
+              fitness Trainers with real-world experience in the field to help
+              you achieve your desired fitness goals. They keeps you motivated
+              through exercise and helps you to improve your proper workout.
+            </p>
+            <p>
+              Yoga Instructor : Our Yoga Instructor help to improve mental
+              health, focus, strength, balance and flexibility.
+            </p>
+            <p>
+              Zumba Teacher : Our Zumba teacher boosts your cardio endurance,
+              Tones the Body & much more with an enjoyable environment
+            </p>
+            <p>
+              Kids Dance Teacher : Our Kids Dance Teacher helps your kid to
+              learn dance as well as make them energetic.
+            </p>
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            p: 1,
-            m: 1,
-            bgcolor: "#ED563B",
-            height: "300px",
-          }}
-        >
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
+        <img
+          src="https://thumbs.dreamstime.com/b/two-attractive-women-exercising-personal-trainers-two-attractive-women-exercising-personal-trainers-gym-122702382.jpg"
+          style={{ width: "400px", height: "300px" }}
+        />
+      </Box>
+      <Box
+        mx="auto"
+        // height="400px"
+        borderRadius="10px"
+        sx={{
+          boxShadow: "5px 5px 25px rgba(0,0,0,0.2)",
+          width: { xs: "400px", md: "1000px" },
+          padding: "20px",
+          display: "flex",
+          marginBottom: "40px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <img
+          src="https://www.gmpfitness.com/wp-content/uploads/2017/12/gmp-fitness-holistic-nutrition-master.jpg"
+          style={{ width: "400px", height: "300px" }}
+        />
+        <Box>
+          <Typography
+            sx={{ padding: "20px", fontWeight: 800, color: "#ed563b" }}
+            variant="h"
           >
-            <Typography>Nutritionist:</Typography>
-            <Typography>
-              A gym session or brisk walk can help. Physical activity stimulates
-              various brain chemicals that may leave you feeling happier, more
-              relaxed and less anxious. You may also feel better about your
-              appearance and yourself when you exercise regularly, which can
-              boost your confidence and improve your self-esteem.
-            </Typography>
-          </Grid>
-          <Grid
-            sx={{
-              flexDirection: "row",
-              p: 1,
-              m: 1,
-              width: "50%",
-            }}
-            xs={12}
-            sm={6}
-          >
-            <img
-              src="https://www.gmpfitness.com/wp-content/uploads/2017/12/gmp-fitness-holistic-nutrition-master.jpg"
-              style={{ width: "95%", height: "90%" }}
-            />
-          </Grid>
+            NUTRITIONIST
+          </Typography>
+          <Typography sx={{ padding: "20px" }}>
+            We have the best certified Nutritionist in our gym, They are
+            specialist in personal fitness training and cannot be undermined.Our
+            Nutrition specialist prepares fitness diet program for each and
+            every member which is followed by the exerciser. The benefit of
+            fitness training program can be seen only if the exerciser strictly
+            follows nutrition program charted out by nutrition specialist.
+          </Typography>
         </Box>
-      </Container>
+      </Box>
     </Box>
   );
 };
