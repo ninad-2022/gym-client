17b16d2022aa7eb267fa72568511b292c9455c43
Omkar Kale <kale2000omkar@gmail.com>
2022-11-20 19:52:40 +0530

    some changes

--- src/layout/blank-layout/BlankLayout.js
+++ src/layout/blank-layout/BlankLayout.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import AppBar from "./NavBar";
-import routes from "../../shared/routes/FrontendRoutes";
-import Home from "../../features/frontend/home/Home";
-import About from "../../features/frontend/about/About";
-import Membership from "../../features/frontend/membership/Membership";
-import Contact from "../../features/frontend/contact/Contact";
+
 import ResponsiveDrawer from "./NavBar";
-import ImageGallery from "../../features/frontend/image-gallery/ImageGallery";
-import GymSchedule from "../../features/frontend/gym-schedule/GymSchedule";
+
 import frontEndRoutes from "../../shared/routes/FrontendRoutes";
 
 const BlankLayout = () => {
--- src/layout/blank-layout/NavBar.js
+++ src/layout/blank-layout/NavBar.js
@@ -7,11 +7,7 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+
 import { Link, NavLink } from "react-router-dom";
 import routes from "../../shared/routes/FrontendRoutes";
 import { styled } from "@mui/styles";
